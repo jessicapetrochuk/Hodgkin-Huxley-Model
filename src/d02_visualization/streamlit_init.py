ee73b2f84ebe36f0135f9eebfb6895e73e360327
Jessica Petrochuk <jesspe@gmail.com>
2021-07-25 17:19:28 -0400

    Renaming activation variable name

--- src/d02_visualization/streamlit_init.py
+++ src/d02_visualization/streamlit_init.py
@@ -4,11 +4,11 @@ import pandas as pd
 def streamlit_init(stimulus_df, membrane_potential_df, n_df, m_df, h_df):
     membrane_voltage_df = pd.DataFrame({"Membrane Voltage": membrane_potential_df})
     stimulus_df = pd.DataFrame({"Stimulus": stimulus_df})
-    n_df = pd.DataFrame({"n": n_df, 'm': m_df, 'h': h_df})
+    activation_variables_df = pd.DataFrame({"n": n_df, 'm': m_df, 'h': h_df})
     st.title('Hodkin Huxley Model')
     st.write('Stimulus vs Time')
     st.line_chart(stimulus_df)
     st.write('Membrane Potential vs. Time')
     st.line_chart(membrane_voltage_df)
     st.write('Activation/Inactivation Variables vs. Time')
-    st.line_chart(n_df)
+    st.line_chart(activation_variables_df)
